fdf0955fce89af5cf28b371cd4529aebf0e3a5c0
zhengwang <zheng.wang.rice@gmail.com>
2018-09-08 19:44:02 -0700

    indent

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ function authorize(filename, filepath, res, callback) {
   const credentials = JSON.parse(process.env.CREDENTIALS);
   const {client_secret, client_id, redirect_uris} = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
-			client_id, client_secret, redirect_uris[0]);
+      client_id, client_secret, redirect_uris[0]);
   const token = process.env.TOKEN;
   oAuth2Client.setCredentials(JSON.parse(token));
   callback(oAuth2Client, filename, filepath, res);
@@ -69,7 +69,7 @@ function authorize(filename, filepath, res, callback) {
 
 function isLoggedIn(req, res, next) {
   if(req.isAuthenticated()) {
-		return next();
+    return next();
   }
   req.flash('error', '你需要登录才能进行操作');
   res.redirect('/login');
@@ -79,23 +79,23 @@ function isLoggedIn(req, res, next) {
 // homepage
 app.get('/', function(req, res) {
   const poems = ['十年一觉扬州梦，赢得青楼薄幸名 --- 杜牧',
-								 '二十四桥明月夜，玉人何处教吹箫 --- 杜牧',
-								 '驰道杨花满御沟，红妆缦绾上青楼 --- 王昌龄',
-								 '香帏风动花入楼，高调鸣筝缓夜愁 --- 王昌龄',
-								 '罗襦宝带为君解，燕歌赵舞为君开 --- 卢照邻'];
+                 '二十四桥明月夜，玉人何处教吹箫 --- 杜牧',
+                 '驰道杨花满御沟，红妆缦绾上青楼 --- 王昌龄',
+                 '香帏风动花入楼，高调鸣筝缓夜愁 --- 王昌龄',
+                 '罗襦宝带为君解，燕歌赵舞为君开 --- 卢照邻'];
   Profile.find({}, function(err, profiles) {
-		if (err) {
-	    console.log(err);
-		} else {
-	    Info.findOne({}, function(err, info) {
-				if (err) {
-					console.log(err);
-				} else {
-					var poem = poems[Math.floor(Math.random()*poems.length)]
-					res.render('index.ejs', {profiles:profiles, info:info, poem: poem});
-				}
-	    });
-		}
+    if (err) {
+      console.log(err);
+    } else {
+      Info.findOne({}, function(err, info) {
+        if (err) {
+          console.log(err);
+        } else {
+          var poem = poems[Math.floor(Math.random()*poems.length)]
+          res.render('index.ejs', {profiles:profiles, info:info, poem: poem});
+        }
+      });
+    }
   });
   // update stat
   updateStat();
@@ -104,65 +104,65 @@ app.get('/', function(req, res) {
 async function updateStat() {
   var today = moment.tz('America/Los_Angeles').format('YYYY/MM/DD UTC');
   Stat.findOne({}, function(err, stat) {
-		if (err) {
-	    console.log(err);
-		} else {
-	    value = stat.homepage.get(today);
-	    if (!value) {
-				stat.homepage.set(today, 1);
-	    } else {
-				stat.homepage.set(today, value+1);
-	    }
-	    stat.save();
-		}
+    if (err) {
+      console.log(err);
+    } else {
+      value = stat.homepage.get(today);
+      if (!value) {
+        stat.homepage.set(today, 1);
+      } else {
+        stat.homepage.set(today, value+1);
+      }
+      stat.save();
+    }
   });    
 }
 
 // admin page
 app.get('/admin', isLoggedIn, function(req, res, err) {
   Profile.find({}, function(err, profiles) {
-		if (err) {
-	    console.log(err);
-		} else {
-	    Info.findOne({}, function(err, info) {
-				if (err) {
-					console.log(err);
-				} else {
-					Stat.findOne({}, function(err, stat) {
-						if (err) {
-							console.log(err);
-						} else {
-							res.render('admin.ejs', {profiles:profiles, info:info, stat: stat});
-						}
-					});
-				}
-	    });
-		}
+    if (err) {
+      console.log(err);
+    } else {
+      Info.findOne({}, function(err, info) {
+        if (err) {
+          console.log(err);
+        } else {
+          Stat.findOne({}, function(err, stat) {
+            if (err) {
+              console.log(err);
+            } else {
+              res.render('admin.ejs', {profiles:profiles, info:info, stat: stat});
+            }
+          });
+        }
+      });
+    }
   });
 });
 
 // edit info GET form
 app.get('/info_update', isLoggedIn, function(req, res) {
   Info.findOne({}, function(err, info) {
-		if (err) {
-	    console.log(err);
-		} else {
-	    res.render('info_update.ejs', {info:info});
-		}
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('info_update.ejs', {info:info});
+    }
   });
 });
 
 // update info PUT route
 app.put('/info_update', isLoggedIn, function(req, res) {
   Info.findOne({}, function(err, info) {
-		req.body.info.show_notice = Boolean(req.body.info.show_notice);
-		req.body.info.allow_thumbs = Boolean(req.body.info.allow_thumbs);
-		info.update(req.body.info, function(err, updated_info) {
-	    if (err) {
-				console.log(err);
-	    }
-		});
-		res.redirect('/admin');
+    req.body.info.show_notice = Boolean(req.body.info.show_notice);
+    req.body.info.allow_thumbs = Boolean(req.body.info.allow_thumbs);
+    info.update(req.body.info, function(err, updated_info) {
+      if (err) {
+        console.log(err);
+      }
+    });
+    res.redirect('/admin');
   });
 });
 
@@ -174,21 +174,21 @@ app.get('/new', isLoggedIn, function(req, res) {
 // create NEW profile post route
 app.post('/new', isLoggedIn, function(req, res) {
   if (req.body.profile.images) {
-		req.body.profile.images = req.body.profile.images.split(';');
+    req.body.profile.images = req.body.profile.images.split(';');
   } else {
-		req.body.profile.images = [];
+    req.body.profile.images = [];
   }
   if (req.body.profile.videos) {
-		req.body.profile.videos = req.body.profile.videos.split(';');
+    req.body.profile.videos = req.body.profile.videos.split(';');
   } else {
-		req.body.profile.videos = [];
+    req.body.profile.videos = [];
   }
   Profile.create(req.body.profile, function(err) {
-		if (err) {
-	    console.log(err);
-		} else {
-	    res.redirect('admin');
-		};
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('admin');
+    };
   });
 });
 
@@ -206,11 +206,11 @@ app.post('/upload_image', isLoggedIn, function(req, res) {
 
   // every time a file has been uploaded successfully,
   form.on('file', function(field, file) {
-		var filepath = path.join(form.uploadDir, file.name);
-		fs.rename(file.path, filepath, (err) => {
-			if (err) throw err;
-		});
-		authorize(file.name, filepath, res, createFile);
+    var filepath = path.join(form.uploadDir, file.name);
+    fs.rename(file.path, filepath, (err) => {
+      if (err) throw err;
+    });
+    authorize(file.name, filepath, res, createFile);
   });
 
   // parse the incoming request containing the form data
@@ -228,16 +228,16 @@ app.post('/upload_video', isLoggedIn, function(req, res) {
   form.uploadDir = './uploads'
   
   if (!fs.existsSync(form.uploadDir)) {
-		fs.mkdirSync(form.uploadDir);
+    fs.mkdirSync(form.uploadDir);
   }
   
   // every time a file has been uploaded successfully,
   form.on('file', function(field, file) {
-		var filepath = path.join(form.uploadDir, file.name);
-		fs.rename(file.path, filepath, (err) => {
-	    if (err) throw err;
-		});
-		authorize(file.name, filepath, res, createVideo);
+    var filepath = path.join(form.uploadDir, file.name);
+    fs.rename(file.path, filepath, (err) => {
+      if (err) throw err;
+    });
+    authorize(file.name, filepath, res, createVideo);
   });
 
   // parse the incoming request containing the form data
@@ -248,19 +248,19 @@ async function createFile(auth, filename, filepath, res) {
   const filesize = fs.statSync(filepath).size;
   const drive = google.drive({version: 'v3', auth});
   const google_res = await drive.files.create({
-		requestBody: {
-	    name: filename,
-	    parents: ['1b839ObCYW_FeOqa9iSok4WUoXN4Kz6x_']
-		},
-		media: {
-	    body: fs.createReadStream(filepath)
-		}
+    requestBody: {
+      name: filename,
+      parents: ['1b839ObCYW_FeOqa9iSok4WUoXN4Kz6x_']
+    },
+    media: {
+      body: fs.createReadStream(filepath)
+    }
   });
   res.send('https://drive.google.com/uc?id='+google_res.data.id)
   fs.unlink(filepath, (err) => {
-		if (err) {
-	    console.log(err);
-		}
+    if (err) {
+      console.log(err);
+    }
   });
 }
 
@@ -269,35 +269,35 @@ async function createVideo(auth, filename, filepath, res) {
   const filesize = fs.statSync(filepath).size;
   const youtube = google.youtube({version: 'v3', auth});
   const google_res = await youtube.videos.insert({
-		part: 'id,snippet,status',
-		notifySubscribers: false,
-		requestBody: {
-	    snippet: {
-				title: 'lilihome-video',
-	    },
-	    status: {
-				privacyStatus: 'public'
-	    }
-		},
-		media: {
-	    body: fs.createReadStream(filepath)
-		}
+    part: 'id,snippet,status',
+    notifySubscribers: false,
+    requestBody: {
+      snippet: {
+        title: 'lilihome-video',
+      },
+      status: {
+        privacyStatus: 'public'
+      }
+    },
+    media: {
+      body: fs.createReadStream(filepath)
+    }
   });
   res.send('https://www.youtube.com/embed/'+google_res.data.id)
   fs.unlink(filepath, (err) => {
-		if (err) {
-	    console.log(err);
-		}
+    if (err) {
+      console.log(err);
+    }
   });
 }
 
 // edit profile GET form
 app.get('/edit/:id', isLoggedIn, function(req, res) {
   Profile.findById(req.params.id, function(err, profile) {
-		if (err) {
-	    console.log(err);
-		} else {
-	    res.render('edit.ejs', {profile: profile});
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('edit.ejs', {profile: profile});
     }
   });
 });
@@ -305,53 +305,53 @@ app.get('/edit/:id', isLoggedIn, function(req, res) {
 // update profile PUT route
 app.put('/:id', isLoggedIn, function(req, res) {
   if (req.body.profile.images) {
-		req.body.profile.images = req.body.profile.images.split(';');
+    req.body.profile.images = req.body.profile.images.split(';');
   } else {
-		req.body.profile.images = [];
+    req.body.profile.images = [];
   }
   if (req.body.profile.videos) {
-		req.body.profile.videos = req.body.profile.videos.split(';');
+    req.body.profile.videos = req.body.profile.videos.split(';');
   } else {
-		req.body.profile.videos = [];
+    req.body.profile.videos = [];
   }
   Profile.findById(req.params.id, function(err, profile) {
-		if (err) {
-	    console.log(err);
-		} else {
-	    var name = profile.name;
-	    if (req.body.profile.name != name) {
-				req.body.profile.likes = 0;
-				req.body.profile.dislikes = 0;
-	    }
-	    Profile.findByIdAndUpdate(req.params.id, req.body.profile, function(err, updated_profile) {
-    		if (err) {
-    		  console.log(err);
-    		} else {
-    		  res.redirect('/admin#' + String(req.params.id));
-    		}
-	    });
-		}
+    if (err) {
+      console.log(err);
+    } else {
+      var name = profile.name;
+      if (req.body.profile.name != name) {
+        req.body.profile.likes = 0;
+        req.body.profile.dislikes = 0;
+      }
+      Profile.findByIdAndUpdate(req.params.id, req.body.profile, function(err, updated_profile) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.redirect('/admin#' + String(req.params.id));
+        }
+      });
+    }
   });
 });
 
 // update profile likes or dislikes
 app.put('/:id/thumbs/:action', function(req, res) {
   Profile.findById(req.params.id, function(err, profile) {
-		if (err) {
-	    console.log(err);
-		} else {
-	    if (req.params.action == 'like') {
-				profile.likes++;
-	    }
-	    if (req.params.action == 'dislike') {
-				profile.dislikes++;
-	    }
-	    Profile.findByIdAndUpdate(req.params.id, profile, function(err, updated_profile) {
-    		if (err) {
-    		  console.log(err);
-				}
-	    });
-		}
+    if (err) {
+      console.log(err);
+    } else {
+      if (req.params.action == 'like') {
+        profile.likes++;
+      }
+      if (req.params.action == 'dislike') {
+        profile.dislikes++;
+      }
+      Profile.findByIdAndUpdate(req.params.id, profile, function(err, updated_profile) {
+        if (err) {
+          console.log(err);
+        }
+      });
+    }
   });
   res.send(null);
 });
@@ -359,11 +359,11 @@ app.put('/:id/thumbs/:action', function(req, res) {
 // destroy profile DELETE route
 app.delete('/:id', isLoggedIn, function(req, res) {
   Profile.findByIdAndRemove(req.params.id, function(err) {
-		if (err) {
+    if (err) {
       console.log(err);
-		} else {
+    } else {
       res.redirect('/admin');
-		}
+    }
   });
 });
 
@@ -374,11 +374,11 @@ app.get('/login', function(req, res) {
 
 // handling login logic
 app.post('/login', passport.authenticate('local',
-																				 {
-    																			 successRedirect: '/admin',
-																					 failureRedirect: '/login',
-																					 failureFlash: '用户名或密码不正确'
-																				 }), function(req, res) {});
+                                         {
+                                           successRedirect: '/admin',
+                                           failureRedirect: '/login',
+                                           failureFlash: '用户名或密码不正确'
+                                         }), function(req, res) {});
 
 // logout route
 app.get('/logout', function(req, res) {
@@ -392,8 +392,8 @@ if (module === require.main) {
   // Start the server
   // [START server]
   const server = app.listen(process.env.PORT || 8080, () => {
-		const port = server.address().port;
-		console.log(`App listening on port ${port}`);
+    const port = server.address().port;
+    console.log(`App listening on port ${port}`);
   });
   // [END server]
 }
--- init.js
+++ init.js
@@ -4,29 +4,29 @@ var Profile = require("./models/profile"),
     Stat    = require('./models/stat');
 
 function initDB() {
-    Info.find({}, function(err, infos) {
-	if (err) throw err;
-	if (infos.length != 1) {
-	    Info.remove({}, function(err){
-		if (err) throw err;
-		Info.create({}, function(err, info){
-		    if (err) throw err;
-		});
-	    });
-	}
-    });
+  Info.find({}, function(err, infos) {
+    if (err) throw err;
+    if (infos.length != 1) {
+      Info.remove({}, function(err){
+        if (err) throw err;
+        Info.create({}, function(err, info){
+          if (err) throw err;
+        });
+      });
+    }
+  });
 
-    Stat.find({}, function(err, stats) {
-	if (err) throw err;
-	if (stats.length != 1) {
-	    Stat.remove({}, function(err) {
-		if (err) throw err;
-		Stat.create({homepage: {}}, function(err, stat){
-		    if (err) throw err;
-		});
-	    });
-	}
-    });
+  Stat.find({}, function(err, stats) {
+    if (err) throw err;
+    if (stats.length != 1) {
+      Stat.remove({}, function(err) {
+        if (err) throw err;
+        Stat.create({homepage: {}}, function(err, stat){
+          if (err) throw err;
+        });
+      });
+    }
+  });
 }
 
 module.exports = initDB;
